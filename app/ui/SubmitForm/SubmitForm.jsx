16fdc8f7505c2290b71f8c7cd66d4da118658790
Kewin Shah <kewinshah25@gmail.com>
2026-02-28 15:53:15 -0800

    Revert "fix attempt #3"
    This reverts commit c0737274bf849772991c1d05f5fea5da406a699a.

--- app/ui/SubmitForm/SubmitForm.jsx
+++ app/ui/SubmitForm/SubmitForm.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./SubmitForm.css";
@@ -80,7 +80,7 @@ const SubmitForm = () => {
     <div className='wrapper'>
       {showBalloon1 && (
         <Image
-          src={`${base}/assets/images/balloon1.png`}
+          src="/assets/images/balloon1.png"
           alt=""
           width={398}
           height={404}
@@ -90,7 +90,7 @@ const SubmitForm = () => {
       )}
       {showBalloon2 && (
         <Image
-          src={`${base}/assets/images/balloon2.png`}
+          src="/assets/images/balloon2.png"
           alt=""
           width={460}
           height={469}
